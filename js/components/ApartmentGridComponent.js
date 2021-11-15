460394d845a68fc0a7ad244c6f117f21e9261e59
Tomas Antanaitis <to.antanaitis@gmail.com>
2021-11-15 18:50:48 +0200

    7: Laukimo animacija ir kortelių grid'as

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -7,7 +7,10 @@ class ApartmentGridComponent {
         this.init();
     }
 
-    fetchApartments = () => API.fetchApartments(this.saveApartments, alert);
+    fetchApartments = () => 
+    setTimeout(() => {
+    API.fetchApartments(this.saveApartments, alert);
+    }, 1000);
 
     saveApartments = (apartments) => {
         this.state.apartments = apartments;
@@ -20,15 +23,31 @@ class ApartmentGridComponent {
         this.state.loading = true;
         this.fetchApartments();
         this.htmlElement = document.createElement('div');
+        this.htmlElement.className = 'row g-3';
+        
         this.render();
+    }
+
+    wrapInColumn = (element) => {
+        const column = document.createElement('div');
+        column.className = 'col-12 col-xs-12 col-sm-6 col-lg-4 col-xl-3';
+        column.appendChild(element);
+        return column;
     }
 
     render = () => {
         const {loading, apartments} = this.state;
         if(loading) {
-            this.htmlElement.innerHTML = 'siunčiama...';
+            this.htmlElement.innerHTML = `<div class="text-center"><img src="../assets/loading.gif"/></div>`
+        } else if(apartments.length > 0){
+            this.htmlElement.innerHTML = '';
+            const apartmentElements = apartments
+            .map(x => new ApartmentCardComponent(x))
+            .map(x => x.htmlElement)
+            .map(this.wrapInColumn)
+            this.htmlElement.append(...apartmentElements);
         } else {
-            this.htmlElement.innerHTML = 'parsiųsta!';
+            this.htmlElement.innerHTML = 'Šiuo metu jokių NT objektų nėra';
         }
     }
 }
