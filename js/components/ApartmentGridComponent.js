3dd5b03a409e267a301d9b3a285fb52fded61758
Tomas Antanaitis <to.antanaitis@gmail.com>
2021-11-15 18:38:31 +0200

    6: Panaudota API.js funkcija visiems būstams gauti

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -1,10 +1,34 @@
 class ApartmentGridComponent {
     constructor() {
+        this.state = {
+            loading: false,
+            apartments: []
+        }
         this.init();
     }
 
+    fetchApartments = () => API.fetchApartments(this.saveApartments, alert);
+
+    saveApartments = (apartments) => {
+        this.state.apartments = apartments;
+        this.state.loading = false;
+
+        this.render();
+    }
+
     init = () => {
+        this.state.loading = true;
+        this.fetchApartments();
         this.htmlElement = document.createElement('div');
-        this.htmlElement.innerHTML = 'Apartamentų komponentas';
+        this.render();
+    }
+
+    render = () => {
+        const {loading, apartments} = this.state;
+        if(loading) {
+            this.htmlElement.innerHTML = 'siunčiama...';
+        } else {
+            this.htmlElement.innerHTML = 'parsiųsta!';
+        }
     }
 }
